aeed25f87af3b5dcf87274ae00f54d73ad85aad4
Walad198 <walad198@yahoo.com>
2020-10-08 14:30:27 +0100

    Changed hello world

--- test03/Program.cs
+++ test03/Program.cs
@@ -6,7 +6,7 @@ namespace test03
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello again World!");
         }
     }
 }
